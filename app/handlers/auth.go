f0e5bd45e85001ea748aef3be6205f88cb39685d
ignoxx <ignoxx@yahoo.de>
2024-08-23 22:17:15 +0200

    init

---
+++ app/handlers/auth.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"lazymeal/app/types"
+
+	"github.com/anthdm/superkit/kit"
+)
+
+func HandleAuthentication(kit *kit.Kit) (kit.Auth, error) {
+	return types.AuthUser{}, nil
+}
